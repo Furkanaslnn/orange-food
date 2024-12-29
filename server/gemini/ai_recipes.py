7904403a07d508a57096c7286eaadc3bb2cfc439
Furkan Aslan <furkanaslan1209@gmail.com>
2024-12-29 10:32:55 +0300

    Refactor AI recipes module to load API key from environment variables and add .env to .gitignore

--- server/gemini/ai_recipes.py
+++ server/gemini/ai_recipes.py
@@ -1,14 +1,13 @@
 import os
 import google.generativeai as genai
-import json  # JSON işlemleri için gerekli modül
+from dotenv import load_dotenv
+import json 
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+load_dotenv()
 
-def main(user_input):
-    genai.configure(api_key="AIzaSyDeaWYdYLlh_aNDls-FiQmTANIIYcUi5cU")
-
-    generation_config = {
+generation_config = {
         "temperature": 1,
         "top_p": 0.95,
         "top_k": 40,
@@ -16,10 +15,15 @@ def main(user_input):
         "response_mime_type": "text/plain",
     }
 
-    model = genai.GenerativeModel(
-        model_name="gemini-2.0-flash-exp",
-        generation_config=generation_config,
-    )
+model = genai.GenerativeModel(
+    model_name="gemini-2.0-flash-exp",
+    generation_config=generation_config,
+)
+
+def main(user_input):
+    load_dotenv()
+    API_KEY = os.getenv("API_KEY")
+    genai.configure(api_key=API_KEY)
 
     chat_session = model.start_chat(
         history=[]
@@ -49,17 +53,14 @@ def main(user_input):
     response = chat_session.send_message(message)
     raw_data = response.text.strip()
 
-    # Başlangıçtaki olası `json` veya benzeri etiketleri kaldır
     if raw_data.startswith("```json"):
-        raw_data = raw_data[7:]  # İlk 7 karakteri atla
+        raw_data = raw_data[7:]  
     if raw_data.endswith("```"):
-        raw_data = raw_data[:-3]  # Son 3 karakteri kaldır
+        raw_data = raw_data[:-3]  
 
-    # JSON formatını doğrula ve parse et
     try:
         data = json.loads(raw_data)
     except json.JSONDecodeError as e:
         raise ValueError("Döndürülen çıktı geçerli bir JSON değil: " + str(e))
 
-    # Sonuç JSON ise döndür
     return data
